88a7a54c8cfdc3edf5409cfdd76de5c3e7d6f93b
savannah-shuhua <kalla0622@gmail.com>
2021-05-08 15:50:45 -0400

    welcome message

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,7 +126,7 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const init = function () {
+const newFeature = function () {
   console.log('Welcome to the application!');
 };
 
@@ -138,6 +138,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome');
+  newFeature();
 };
 init();
