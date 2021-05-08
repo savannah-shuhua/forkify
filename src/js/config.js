8e60ec9aef98c0de65186a826b9893c895dd9e92
savannah-shuhua <kalla0622@gmail.com>
2021-05-08 13:53:38 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0abd5dc0-f67a-446d-a767-5daf6d431129';
+export const MODAL_CLOSE_SEC = 2.5;
